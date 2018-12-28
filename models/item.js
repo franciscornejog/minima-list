ba5efd1c0f983ebd7628514c0b1977cf4272d824
fcornejog <40514214+fcornejog@users.noreply.github.com>
2018-12-28 10:45:07 -0800

    Refactor item schema

--- models/item.js
+++ models/item.js
@@ -4,11 +4,8 @@ var mongoose = require("mongoose");
 // Item Schema Setup
 var itemSchema = new mongoose.Schema({
     name: String,
-    image: String,
     description: String,
     quantity: Number,
-    location: String,
-    tag: String,
     notes: String,
     created: 
     {
