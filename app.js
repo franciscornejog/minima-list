f32af834f8506e64778fe28c1a633ef1aff7c274
Francisco Cornejo <cornejogarcia@Franciscos-MacBook-Air.local>
2019-06-04 17:50:52 -0700

    Add changes

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
 // Packages
-var passport_local_mongoose = require("passport-local-mongoose"),
-    local_strategy          = require("passport-local"),
-    passport                = require("passport"),
-    flash                   = require("connect-flash"),
-    body_parser             = require("body-parser"),
-    moment                  = require("moment"),
-    method_override         = require("method-override"),
-    mongoose                = require("mongoose"),
-    express_sanitizer       = require("express-sanitizer"),
-    express                 = require("express"),
-    app                     = express();
+const passport_local_mongoose = require('passport-local-mongoose');
+const localStrategy = require('passport-local');
+const passport = require('passport');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const expressSanitizer = require('express-sanitizer');
+const express = require('express');
 
-// Models 
-var Item = require("./models/item"),
-    User = require("./models/user");
+const app = express();
 
-// Routes 
-var indexRoutes      = require("./routes/index"),
-    collectionRoutes = require("./routes/collection");
+// Models
+const Item = require('./models/item');
+const User = require('./models/user');
+
+// Routes
+const indexRoutes = require('./routes/index');
+const collectionRoutes = require('./routes/collection');
 
 // Database Connection
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/minimalist";
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+const url = process.env.DATABASEURL || 'mongodb://localhost:27017/minimalist';
 mongoose.connect(url);
 
 // Standard Setup
-app.set("view engine", "ejs");
-app.use(body_parser.urlencoded({extended: true}));
-app.use(method_override("_method"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(flash());
 
 // Express Setup
-app.use(express.static("public"));
-app.use(express_sanitizer());
-app.use(require("express-session")({
-    secret: "some_secret",
-    resave: false,
-    saveUninitialized: false
+app.use(express.static('public'));
+app.use(expressSanitizer());
+app.use(require('express-session')({
+  secret: 'some_secret',
+  resave: false,
+  saveUninitialized: false,
 }));
 
 // Passport Setup
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new local_strategy(User.authenticate()));
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Global Variables Setup
-app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
 });
 
 // Routes Setup
 app.use(indexRoutes);
-app.use("/collection", collectionRoutes);
+app.use('/collection', collectionRoutes);
 
 // Server Connection
-var port = process.env.PORT || 3000;
-var ip = process.env.IP || "";
-app.listen(port, ip, function() {
-    console.log("Minimalist Online");
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || '';
+app.listen(port, ip, () => {
+  console.log('Minimalist Online');
 });
-
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,19 @@
 /* Toggle menu when user clicks on button */
 function dropdown() {
-    document.getElementById("dropdown-content").classList.toggle("show");
+  document.getElementById("dropdown-content").classList.toggle("show");
 }
 
 /* Close menu when user clicks beside menu */
 window.onclick = function(e) {
-    if(!e.target.matches(".dropbtn")) {
-        var dropdown = document.getElementByClassName("dropdown-content");
-        for(var i = 0; i < dropdown.length; i++) {
-            var openDropdown = dropdown[i];
-            if(openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");
-            }
-        }
+  if(!e.target.matches(".dropbtn")) {
+    var dropdown = document.getElementByClassName("dropdown-content");
+    for(var i = 0; i < dropdown.length; i++) {
+      var openDropdown = dropdown[i];
+      if(openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
+  }
 }
 
 
